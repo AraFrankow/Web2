43895b53aed4dc14c62d8d8c9be9f0126e87923e
AraFrankow <ara.frankow@gmail.com>
2024-11-21 02:55:49 -0300

    fin3.0

--- admin/modi/mod_cat.php
+++ admin/modi/mod_cat.php
@@ -1,24 +1,84 @@
 <?php
 include_once("../../componentes/conf/conf.php");
-$id;
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $consulta = "SELECT * FROM `habitacion` WHERE `id_Habitacion` = '$id' ";
-    $resultado = mysqli_query($con,$consulta);
-}
-
 ?>
-
-<form action="mod_cat_ok.php" method="get">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear - Admin</title>
+    <link rel="stylesheet" href="../../asset/css/bootstrap.css">
+    <link rel="stylesheet" href="../../asset/css/estilos.css">
+    <link rel="stylesheet" href="../../asset/css/estilos.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg color">
+            <div class="container-fluid">    
+                <h1><a class="navbar-brand letra" href="../index.php" >Crear - Admin</a></h1>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class='navbar-nav'>
+                        <li class='nav-item' >
+                            <a href='../componentes/security/logout.php' class='nav-link letra' >Cerrar Sesión</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+<main>
     <?php
-        while($fila = mysqli_fetch_array($resultado)){
-            print "
-                <input type=hidden name=id value=$fila[id_Habitacion] >
-                <input type=text name=tipo value=$fila[tipo] >
-                <input type=text name=precio value=$fila[precio] >
-                <input type=text name=estado value=$fila[fk_Estado] >
-                <input type=submit value=Modificar> 
-            ";
-        }
+    $id;
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $consulta = "SELECT * FROM `habitacion` WHERE `id_Habitacion` = '$id' ";
+        $resultado = mysqli_query($con,$consulta);
+    }
     ?>
-</form>
+    <form action="mod_cat_ok.php" method="get">
+        <?php
+            while($fila = mysqli_fetch_array($resultado)){
+                print "
+                <section class='row abajo'>
+                    <article class='col-5 mover'>    
+                        <input type=hidden name=id value=$fila[id_Habitacion] >    
+                        <div>
+                            <label for='habitacion' class='form-label letra'>Tipo de habitación</label>
+                            <input type=text name=tipo value=$fila[tipo] >
+                        </div>
+                        <div>
+                            <label for='habitacion' class='form-label letra'>Precio</label>
+                            <input type=text name=precio value=$fila[precio] >
+                        </div>
+                        <div>
+                            <label for='habitacion' class='form-label letra'>Imagen de la habitación</label>
+                            <input id='media' name='media' type='file' class='form-control'>
+                        </div>
+                        <div>
+                            <label for='habitacion' class='form-label letra'>Estado de la habitación</label>
+                            <input type=text name=estado value=$fila[fk_Estado] >
+                        </div>
+                        <input type=submit value=Modificar class='btn btn-primary'> 
+                    </article>
+                </section>
+                ";
+            }
+        ?>
+    </form>
+</main>
+
+<footer>
+        <nav class="navbar fixed-bottom color">
+            <div class="container-fluid">
+                <p class="letra pt-3">Arabela Frankow &copy;</p>
+            </div>
+        </nav>
+        
+    </footer>
+    
+    <script src="../../asset/js/bootstrap.js" ></script>
+</body>
+</html>
--- admin/modi/mod_cat_ok.php
+++ admin/modi/mod_cat_ok.php
@@ -3,14 +3,16 @@ include_once("../../componentes/conf/conf.php");
 $tipo;
 $id;
 $precio;
+$media;
 $estado;
-if (isset($_GET['tipo']) && isset($_GET['id']) && isset($_GET['precio']) && isset($_GET['estado'])) {
+if (isset($_GET['tipo']) && isset($_GET['id']) && isset($_GET['precio']) && isset($_GET['estado']) && isset($_GET['media'])) {
     $tipo = $_GET['tipo'];
     $id = $_GET['id'];
     $precio = $_GET['precio'];
     $estado = $_GET['estado'];
+    $media = $_GET['media'];
 
-    $consulta = "UPDATE habitacion SET tipo='$tipo', precio='$precio', fk_Estado='$estado' WHERE id_Habitacion = '$id' ";
+    $consulta = "UPDATE habitacion SET tipo='$tipo', precio='$precio', fk_Estado='$estado',media ='$media'  WHERE id_Habitacion = '$id' ";
     mysqli_query($con,$consulta);
     header("Location: ../index.php ");
 }
